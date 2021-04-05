c594e44f34b37449fa042a79a467bac347887ff6
Sydney Evans <sydneye143@gmail.com>
2021-04-05 16:17:16 -0500

    first_pythonass

---
+++ fundamentals/hello_world.py
@@ -0,0 +1,15 @@
+print('Hello World!')
+name= "Sydney"
+print("Hello", name)
+print('Hello'+ name)
+
+name=42
+print('Hello, 42')
+
+ print('Hello+ name')
+
+
+fav_food1="sushi"
+fav_food2= "pizza"
+print("I love too eat {} and {}" .format(fav_food1,fav_food2))
+print(f" I love too eat {fav_food1} and {fav_food2}")
